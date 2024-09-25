5f15da79d821f18eace3d5c3d2d009168ad4bf42
arhim0use <chingisbek@gmail.com>
2024-09-25 19:55:47 +0300

    feleciag упрощение инициализации, бриф к структуре, добавлен хедер для хелп функций

---
+++ src/s21_Maze_Model/maze_model_helper.h
@@ -0,0 +1,10 @@
+#ifndef MAZE_MODEL_HELPER_H
+#define MAZE_MODEL_HELPER_H
+
+#include "../C_Matrix/s21_matrix.h"
+#include "maze_model.h"
+
+
+maze_errors init_random_matrix(rectangular_maze *A);
+
+#endif // MAZE_MODEL_HELPER_H
